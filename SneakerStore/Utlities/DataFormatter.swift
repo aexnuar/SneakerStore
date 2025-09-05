173b605bb80257859f2b6e0784000bcf201b5209
Aleksandr <lef0791@gmail.com>
2025-09-05 18:29:18 +0300

    Добавлена новая группа с Утилитами и классом DataFormatter с методами для форматирования строки и прайса. Применено в контроллере корзины и в контролере избранного

---
+++ SneakerStore/Utlities/DataFormatter.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  SneakerStore
+//
+//  Created by aex on 05.09.2025.
+//
+
+import Foundation
+
+class dataFormatter {
+    static let shared = dataFormatter()
+    
+    
+}
