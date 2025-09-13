1a1fd416c53b494980b688d8d70e1bc88d88fd44
Aleksandr <lef0791@gmail.com>
2025-09-13 17:53:42 +0300

    Добвален новый пустой экран корзины

---
+++ SneakerStore/ViewControllers/EmptyCartViewController.swift
@@ -0,0 +1,8 @@
+//
+//  EmptyCartViewController.swift
+//  SneakerStore
+//
+//  Created by aex on 13.09.2025.
+//
+
+import Foundation
---
+++ SneakerStore/Views/EmptyCartView.swift
@@ -0,0 +1,8 @@
+//
+//  EmptyCartView.swift
+//  SneakerStore
+//
+//  Created by aex on 13.09.2025.
+//
+
+import Foundation
