7275fd5b93d32530a25b3400846cf33cca85da7a
Aleksandr <lef0791@gmail.com>
2025-09-24 14:39:25 +0300

    получение данных через json

---
+++ SneakerStore/Services/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkService.swift
+//  SneakerStore
+//
+//  Created by aex on 23.09.2025.
+//
+
+import Foundation
+
---
+++ SneakerStore/Services/URLManager.swift
@@ -0,0 +1,8 @@
+//
+//  URLManager.swift
+//  SneakerStore
+//
+//  Created by aex on 23.09.2025.
+//
+
+import Foundation
